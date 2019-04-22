4da165622045e7e0ce1456bb083f3669c23979a0
ia.miroshkin <barb0s9109@mail.ru>
2019-04-22 15:08:37 +0300

    create server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('dist'));
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, './dist/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening 3000');
+});
