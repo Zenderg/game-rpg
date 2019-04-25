ca451ae2e401356c4216c9116249d5697ee2d2b4
dm.kuzin <danil54658798@gmail.com>
2019-04-25 13:23:31 +0300

    try create map 1

--- src/js/scene/preload.js
+++ src/js/scene/preload.js
@@ -1,7 +1,7 @@
 function preload() {
-  this.load.image('bg', 'assets/image1.jpg');
-  this.load.image('platform', 'assets/platform.png');
-  this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 45 });
+  this.load.image('tiles', 'assets/my-tiles.png');
+  this.load.atlas('atlas', 'https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/atlas/atlas.png', 'https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/atlas/atlas.json');
+  this.load.tilemapTiledJSON('map', 'assets/new-new.json');
 }
 
 export default preload;
