fbc43c656845b3dcaa582efd555d122d7d9872d7
Zenderg <danil54658798@gmail.com>
2019-05-15 16:50:18 +0300

    Delete preload.js

--- src/js/scene/preload.js
+++
@@ -1,7 +0,0 @@
-function preload() {
-  this.load.image('tiles', 'assets/my-tiles.png');
-  this.load.atlas('atlas', 'https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/atlas/atlas.png', 'https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/atlas/atlas.json');
-  this.load.tilemapTiledJSON('map', 'assets/new-new.json');
-}
-
-export default preload;
