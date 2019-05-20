a9d1c0af0b71855260e9d7ab65f331e9edb5e85a
Zenderg <danil54658798@gmail.com>
2019-05-20 21:04:58 +0300

    hotfix

--- src/classes/Anna.ts
+++ src/classes/Anna.ts
@@ -6,31 +6,23 @@ interface hp {
 }
 
 export default class Anna extends Creature {
-    private _hp: hp = {current: 0, max: 0};
+    private hp: hp = {current: 0, max: 0};
     private hpBar: Phaser.GameObjects.Graphics;
 
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frames?: string | number) {
         super(scene, x, y, texture, frames);
-        this._hp.max = 10;
-        this._hp.current = 10;
+        this.hp.max = 10;
+        this.hp.current = 10;
         this.hpBar = scene.add.graphics({
             fillStyle: {
                 color: 0x15fa03
             }
         });
-        (<any>Object).observe(this._hp, this.updateHpBar);
+        (<any>Object).observe(this.hp, this.updateHpBar);
     }
 
     private updateHpBar = () => {
-        this.hpBar.fillRect(400, 400, (this._hp.current / this._hp.max) * 10, 5).setDepth(2);
+        this.hpBar.fillRect(400, 400, (this.hp.current / this.hp.max) * 10, 5).setDepth(2);
         console.log(11111111111111111111111111111);
     };
-
-    get hp(): number {
-        return this._hp;
-    }
-
-    set hp(hp:{current:number, max?:number}) {
-        this._hp = {...this.hp, hp};
-    }
 }
