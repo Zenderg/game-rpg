378d4988e431ffd41ec62c8894f8274c551a45cc
Zenderg <danil54658798@gmail.com>
2019-05-25 01:09:01 +0300

    fix window size

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import {LoadScene} from "./scenes/LoadScene";
 import {MenuScene} from "./scenes/MenuScene";
 import {PlayScene} from "./scenes/PlayScene";
 
-let game = new Phaser.Game({
-    width: 800,
-    height: 600,
+const ratio = Math.max(window.innerWidth / window.innerHeight, window.innerHeight / window.innerWidth);
+const DEFAULT_HEIGHT = 630; // any height you want
+const DEFAULT_WIDTH = ratio * DEFAULT_HEIGHT;
+
+const game = new Phaser.Game({
     scene:[
         LoadScene, MenuScene, PlayScene
     ],
@@ -20,6 +22,9 @@ let game = new Phaser.Game({
         }
     },
     scale:{
-        mode: Phaser.Scale.FIT
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT
     }
 });
