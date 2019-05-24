d92745ec4d2a65311e693c5f17046820990d3b1f
Zenderg <danil54658798@gmail.com>
2019-05-24 21:42:07 +0300

    fix hp bar on camera

--- src/classes/Anna.ts
+++ src/classes/Anna.ts
@@ -14,18 +14,10 @@ export default class Anna extends Creature {
     private hpBar: Phaser.GameObjects.Graphics;
     hpWatcher = new Proxy(this.hp, {
        get: (target:any, prop:string) => {
-           console.log("getter");
-           console.log(target);
-           console.log(prop);
            return target[prop];
        },
         set: (target:any, prop:string, value:number) => {
-            console.log("setter");
-            // console.log(target);
-            // console.log(prop);
-            // console.log(value);
             if(this.checkDeath()) return true;
-            console.log(this.checkDeath());
             target[prop] = value;
             this.updateHpBar();
             return true;
@@ -39,9 +31,6 @@ export default class Anna extends Creature {
        this.updateHpBar();
     }
     private updateHpBar = () => {
-        console.log(11111111111111111111111111111);
-        console.log(this.hp.current, this.hp.max);
-        console.log((this.hp.current / this.hp.max) * 100);
         if(this.hpBar) {
             this.scene.sys.updateList.remove(this.hpBar);
             this.scene.sys.displayList.remove(this.hpBar);
@@ -51,7 +40,8 @@ export default class Anna extends Creature {
                 color: 0x15fa03
             }
         });
-        this.hpBar.fillRect(400, 400, (this.hp.current / this.hp.max) * 100, 5).setDepth(2);
+        this.hpBar.fillRect(25, 10, (this.hp.current / this.hp.max) * 100, 5).setDepth(2);
+        this.hpBar.setScrollFactor(0, 0);
     };
 
     private checkDeath = () => {
